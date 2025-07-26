eb6ebbef66b54af62c6e749c7c840c8fc0ffd593
akhi1817 <akhileshkumbhar2207@gmail.com>
2025-07-26 20:25:48 +0530

    route problem check

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -7,6 +7,10 @@ router.post('/create-todo',createTodo);
 router.put('/update-todo/:id',updateTodo);
 router.delete('/delete-todo/:id',deleteTodo);
 
+router.get('/debug', (req, res) => {
+  res.send('API routes are working');
+});
+
 
 
 module.exports=router;
