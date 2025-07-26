86627c3e8550f3aa81e0fd67ca26eee1d5a4fc57
akhi1817 <akhileshkumbhar2207@gmail.com>
2025-07-26 21:00:22 +0530

    port changed applisten

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get('/',(req,res)=>{
     res.send('hello world');
 })
 
-app.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`);
-})
+// app.listen(PORT,()=>{
+//     console.log(`server is running on ${PORT}`);
+// })
